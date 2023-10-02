6e1e6eec4aeab1e48844a38131dc378a322098ae
karto-dev <63463182+karto-dev@users.noreply.github.com>
2023-10-02 15:25:41 +0000

    Latest Commit Changes

--- learnings/functions.js
+++ learnings/functions.js
@@ -1,9 +1,37 @@
 function mys(name = "bdf") {
   return `${name} Morning`;
 }
-console.log(mys("Karto"));
+//console.log(mys("Karto"));
 
 function mys(...numbers) {
   return `${numbers} Morning`;
 }
-console.log(mys(100, 200, 300));
+// console.log(mys(100, 200, 300));
+
+const userkar = {
+  username: "karto",
+  price: 100,
+  welcomef: function () {
+    console.log(`${this.username} , Welcome`);
+    console.log(this);
+  },
+};
+// userkar.welcomef();
+// userkar.username = "san";
+userkar.welcomef();
+
+// function full() {
+//   console.log(this);
+//   //global data
+// }
+// full();
+
+// const newFun = () => {
+//   const username = " jshbhb";
+//   console.log(username);
+// };
+// newFun();
+
+// implicit arrow function
+const aadTwo = (num1, num2) => num1 + num2;
+console.log(aadTwo(3, 4));
