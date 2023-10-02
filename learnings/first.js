0cb975d330cbc04a96eecde1b0d95bcd6cf81ac9
karto-dev <63463182+karto-dev@users.noreply.github.com>
2023-10-02 06:21:38 +0000

    Latest Changes

--- learnings/first.js
+++ learnings/first.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+console.log(nam.slice(0,5))
+const Syn = Symbol("key1")
+const User = {
+    name : "Karthik",
+    "full Name":"Karto C",
+    age : "18",
+    location : "NBng",
+    days : ["Monday","TuesDay"],
+    [Syn] : "newKet"
+
+}
+console.log(User.name)
+console.log(User[Syn])
+
+
+
+
