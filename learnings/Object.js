363d3826702fb83020ad54eceae9b19a5ebb3ac5
karto-dev <63463182+karto-dev@users.noreply.github.com>
2023-10-02 07:09:10 +0000

    latest Changes

---
+++ learnings/Object.js
@@ -0,0 +1,36 @@
+const newUser = {};
+newUser.id = "123";
+newUser.email = "abdx.gmail.com";
+newUser.instruct = "Karto";
+console.log(newUser);
+
+const regularUser = {
+  email: "abs@gmail.com",
+  fullame: {
+    userfullname: {
+      firstname: "Karto",
+      lastname: "devops",
+    },
+  },
+};
+
+// console.log(regularUser.fullname?.userfullname.firstname); // to avoid errors and to check Key exists add ? marks for the Keys
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+const obj4 = { 5: "a", 6: "b" };
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+//{} is the Target object otheres are the Source
+const obj3 = { ...obj1, ...obj2 };
+// console.log(obj3);
+
+// console.log(Object.keys(newUser));
+// console.log(Object.values(newUser));
+// console.log(Object.entries(newUser));
+
+// console.log(newUser.hasOwnProperty("email"));
+
+const { email: em } = newUser;
+console.log(em);
--- learnings/first.js
+++ learnings/first.js
@@ -1,17 +1,20 @@
-console.log(nam.slice(0,5))
-const Syn = Symbol("key1")
+const Syn = Symbol("key1");
 const User = {
-    name : "Karthik",
-    "full Name":"Karto C",
-    age : "18",
-    location : "NBng",
-    days : ["Monday","TuesDay"],
-    [Syn] : "newKet"
+  name: "Karthik",
+  "full Name": "Karto C",
+  age: "18",
+  location: "NBng",
+  days: ["Monday", "TuesDay"],
+  [Syn]: "newKet",
+};
+console.log(User.name);
+console.log(User[Syn]);
 
-}
-console.log(User.name)
-console.log(User[Syn])
+User.name = "Karto";
+//Object.freeze(User);will not allow to add anything to object
+console.log(User);
 
-
-
-
+User.greeting = function () {
+  console.log(`hello Morning , ${this.days}`);
+};
+console.log(User.greeting());
